768ce1e60be8699a4800e678b5c4ac610e0cd350
Matt Lo <mattlo.developer@gmail.com>
2013-10-22 15:05:41 -0500

    added interface for management

--- src/index.php
+++ src/index.php
@@ -14,21 +14,51 @@
 			<div class="input-form">
 				<section>
 					<h2>Configuration</h2>
+					<p>
+						This tool expects that all URLs live in the same domain. Your-mile-may-vary for multi-domain support in a single process.
+					</p>
 					<div class="input-group input-url-list">
 						<span class="input-group-addon"><strong>URL(s)</strong><br />Delimited by line breaks</span>
 						<textarea class="form-control"></textarea>
 					</div>
+					
+					<h4>Advanced Options</h4>
+					<div class="input-group input-asset-prefix">
+						<span class="input-group-addon">Asset Prefix</span>
+						<input type="text" class="form-control" />
+					</div>
+					<div class="input-group input-html-output">
+						<span class="input-group-addon">Output as .HTML files</span>
+						<span class="form-control">
+							<input type="checkbox" value="/output" />
+						</span>
+					</div>
 					<div class="input-group pull-right">
-						<button type="submit" class="btn btn-primary">Review</button>
+						<a href="" class="restart-btn">Restart</a> <button type="submit" class="btn btn-primary review-btn">Review</button>
 					</div>
 				</section>
 				<section>
 					<h2>Confirm</h2>
+					<div class="input-group confirm-url-count">
+						<span class="input-group-addon">Amount of URLs</span>
+						<span class="form-control"></span>
+					</div>
+					<div class="input-group confirm-asset-prefix">
+						<span class="input-group-addon">Asset Prefix</span>
+						<span class="form-control"></span>
+					</div>
+					<div class="input-group confirm-output-html">
+						<span class="input-group-addon">Output as HTML</span>
+						<span class="form-control"></span>
+					</div>					
+					<div class="input-group pull-right">
+						<a href="" class="restart-btn">Restart</a> <button type="submit" class="btn btn-primary run-btn">Run</button>
+					</div>
 				</section>
 			</div>
 
 			<div class="console">
-
+				
 			</div>
 		</div>
 
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -19,16 +19,144 @@ App.Constants = (function () {
 	};
 }());
 
+App.Enums = (function () {
+	'use strict';
+	
+	return {
+		SUCCESS: 0,
+		PROCESSING: 1,
+		FAILED: 2,
+		QUEUED: 3
+	};
+}());
+
+App.Url = (function () {
+	'use strict';
+	
+	var WARNING_CODES = [
+		{clsName: 'success', label: 'Downloaded'},
+		{clsName: 'primary', label: 'Processing...'},
+		{clsName: 'danger', label: 'Failed'},
+		{clsName: 'default', label: 'Queued'}
+	];
+	
+	function Url(url, status, index) {
+		var fakeNode = $('<a />')[0];
+		fakeNode.href = url;
+		
+		this.url = fakeNode;
+		this.status = status;
+		this.index = index;
+	}
+	
+	Url.prototype = {
+		generateStatus: function(status) {
+			this.status = status;
+			
+			var path = this.url.pathname;
+			
+			if (App.Global.Config.saveAsHtmlExt === true) {
+				path = path.replace(/\.[^/.]+$/, '.html');
+			}
+			
+			return '<a href="' + App.Global.Config.assetPrefix + path + '" target="_blank" class="url">' + App.Global.Config.assetPrefix + this.url.pathname + '</a> ' +
+				'<span class="label label-' + WARNING_CODES[this.status].clsName + '">' +WARNING_CODES[this.status].label + '</span>';
+		}
+	};
+	
+	return Url;
+}());
+
+App.Request = (function () {
+	'use strict';
+	
+	return function () {
+		var url = App.Global.Config.urlList.shift();
+		
+		$('.console > div').eq(url.index).html(url.generateStatus(App.Enums.PROCESSING));
+		
+		// @TODO refactor request. also use `data` prop. (disable URI encode on url but the rest is is fine)
+		$.ajax({
+			url: '/src/app?url=' + url.url + '&srcroot=' + App.Global.Config.assetPrefix + (App.Global.Config.saveAsHtmlExt === true ? '&htmlext=true' : '')
+		}).success(function() {
+			$('.console > div').eq(url.index).html(url.generateStatus(App.Enums.SUCCESS));
+		}).fail(function () {
+			$('.console > div').eq(url.index).html(url.generateStatus(App.Enums.FAILED));
+		}).done(function () {
+			if (App.Global.Config.urlList.length > 0) {
+				App.Request();
+			}
+		});
+	}
+}());
+
+App.Global = {
+	Config: {
+		/**
+		 * @type {String}
+		 */
+		assetPrefix: '/output',
+		/**
+		 * @type {Array}
+		 */
+		urlList: [],
+		/**
+		 * @type {Boolean}
+		 */
+		saveAsHtmlExt: false
+	}
+};
+
 // Main Process
 (function () {
 	'use strict';
 
 	$(document).ready(function () {
 		// bind all step sections
-		var sections = $(App.Constants.SECTIONS_CSS_SELECTOR);
+		var sections = $(App.Constants.SECTIONS_CSS_SELECTOR),
+			consoleOut = $('.console'),
+			i;
 		
 		// show first one
 		sections.eq(0).slideDown(App.Constants.SECTION_ANIMATION_DURATION);
+		
+		$('.review-btn').click(function () {
+			var urls = $('.input-url-list textarea').val().split('\n'),
+				assetPrefix = $('.input-asset-prefix input').val(),
+				saveAsHtmlExt = $('.input-html-output input').prop('checked');
+				
+			// iterate over lines
+			for (i = 0; i < urls.length; ++i) {
+				App.Global.Config.urlList.push(new App.Url(urls[i], App.Enums.QUEUED, i));
+			}
+			
+			// save options
+			App.Global.Config.assetPrefix = assetPrefix;
+			App.Global.Config.saveAsHtmlExt = saveAsHtmlExt;
+			
+			// update review display
+			$('.confirm-url-count .form-control').text(App.Global.Config.urlList.length);
+			$('.confirm-asset-prefix .form-control').text(App.Global.Config.assetPrefix);
+			$('.confirm-output-html .form-control').text(App.Global.Config.saveAsHtmlExt === true ? 'Yes' : 'No');
+			
+			// continue to review
+			sections.eq(0).slideUp(App.Constants.SECTION_ANIMATION_DURATION);
+			sections.eq(1).slideDown(App.Constants.SECTION_ANIMATION_DURATION);
+		});
+		
+		// final confirm button trigger
+		$('.run-btn').click(function () {
+			// hide button
+			$('.input-group.pull-right, h2', sections.eq(1)).hide();
+			
+			// update console
+			for (i = 0; i < App.Global.Config.urlList.length; ++i) {
+				console.log(App.Global.Config.urlList[i]);
+				consoleOut.append('<div>' + App.Global.Config.urlList[i].generateStatus(App.Enums.QUEUED) + '</div>');
+			}
+			
+			App.Request();
+		});
 	});
-
 }());
+
